7ec91054b4cad8b87d41188259fea7ffdb45b559
miftah1299 <jannat27120@gmail.com>
2024-12-24 14:18:39 +0600

    add MongoDB connection and API endpoint for marathon events

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -12,8 +12,8 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://job-portal-cd467.web.app",
-            "https://job-portal-cd467.firebaseapp.com",
+            "https://marathon-event.web.app",
+            "https://marathon-event.firebaseapp.com",
         ],
         credentials: true,
     })
@@ -21,10 +21,49 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fh7he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+
+        const marathonEventCollection = client
+            .db("marathonEventDB")
+            .collection("marathonEvents");
+
+        console.log("Connected to marathonEventDB");
+
+        // GET all marathon events
+        app.get("/marathonEvents", async (req, res) => {
+            const marathonEvents = await marathonEventCollection
+                .find({})
+                .toArray();
+            res.send(marathonEvents);
+        });
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Job Portal is running...");
+    res.send("Marathon Event API is running...");
 });
 
 app.listen(port, () => {
